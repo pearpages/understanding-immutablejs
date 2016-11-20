3dfbdea35c845de17922cca924f6dc12be70ac83
pearpages <pere@soms.cat>
2016-11-20 10:54:14 +0100

    chai and mocha up and running

---
+++ src/test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+describe('whatever', function () {
+    it('should ...', function () {
+        expect(false).to.equal(false);
+    });
+});
